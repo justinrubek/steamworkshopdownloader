a114098dfd620a9b7a465b92a05fc15703d66723
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-05-15 21:39:19 -0500

    chore: add nix devShell with python

---
+++ flake-parts/python.nix
@@ -0,0 +1,30 @@
+{inputs, ...}: {
+  perSystem = {
+    config,
+    pkgs,
+    system,
+    inputs',
+    self',
+    ...
+  }: let
+    python-packages = (
+      ps:
+        with ps; [
+          requests
+          tkinter
+          configparser
+        ]
+    );
+    python = pkgs.python3.withPackages python-packages;
+  in {
+    packages = {
+      inherit python;
+    };
+
+    devShells.default = pkgs.mkShell rec {
+      packages = [
+        python
+      ];
+    };
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A rust project";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux"];
+      imports = [
+        ./flake-parts/python.nix
+      ];
+    };
+}
