9a8d5c68c09da0ef9361331c0c1242ce75aedbb1
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-05-15 21:51:26 -0500

    style: Configure pre-commit-hooks with treefmt

--- flake-parts/python.nix
+++ flake-parts/python.nix
@@ -25,6 +25,10 @@
       packages = [
         python
       ];
+
+      shellHook = ''
+        ${config.pre-commit.installationScript}
+      '';
     };
   };
 }
--- flake.nix
+++ flake.nix
@@ -4,13 +4,21 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs:
     inputs.flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux" "aarch64-linux"];
       imports = [
+        inputs.pre-commit-hooks.flakeModule
+
         ./flake-parts/python.nix
+        ./flake-parts/pre-commit.nix
+        ./flake-parts/formatting.nix
       ];
     };
 }
